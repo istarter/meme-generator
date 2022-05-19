6bd7700d25f18fa7c11999d42891837b3f6796dd
istarter <najibkhan.bahrian@gmail.com>
2022-05-19 17:30:41 +0500

    pickup random url and

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,8 +1,21 @@
 import React from "react";
 import MamesData from "../MemesData";
+
 const Meme = () => {
   const getMemeImage = () => {
-    alert("click");
+    const memesArray = MamesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    // console.log(url);
+  };
+
+  const thingsArray = ["Thing 1", "Thing 2"];
+  const thingsElements = thingsArray.map((thing) => <p key={thing}>{thing}</p>);
+  //   console.log(thingsElements);
+  const addItem = () => {
+    const newThing = `Thing ${thingsArray.length + 1}`;
+    thingsArray.push(newThing);
+    console.log(thingsArray);
   };
 
   return (
@@ -10,7 +23,7 @@ const Meme = () => {
       <div className="form">
         <input type={"text"} className="form-input" placeholder="Top Text" />
         <input type={"text"} className="form-input" placeholder="Bottom Text" />
-        <button onClick={getMemeImage} className="form-button">
+        <button onClick={addItem} className="form-button">
           Get a new meme image
         </button>
       </div>
