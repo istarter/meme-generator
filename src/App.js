a576ac43cb6c0b138411f74cdbcec0952e811029
istarter <najibkhan.bahrian@gmail.com>
2022-05-23 20:55:11 +0500

    add previouse state using spread operater

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ import Meme from "./components/Meme";
 
 function App() {
   const [isGoingOut, setIsGoingOut] = useState(true);
-
+  const [thingsArray, setThingsArray] = useState(["Thing1", "Thing2"]);
   const changeMind = () => {
     setIsGoingOut((prevState) => !prevState);
   };
+
+  const addItem = () => {
+    setThingsArray((preveState) => [
+      ...preveState,
+      `Thing ${preveState.length + 1}`,
+    ]);
+  };
+  const thingsElement = thingsArray.map((thing) => <p key={thing}>{thing}</p>);
 
   return (
     <div>
@@ -17,6 +25,10 @@ function App() {
       <div onClick={changeMind} className="state">
         <h1 className="state-title">Do I feel like going out tonight?</h1>
         <h1>{isGoingOut ? "Yes" : "No"}</h1>
+      </div>
+      <div>
+        <button onClick={addItem}>Add Item</button>
+        {thingsElement}
       </div>
     </div>
   );
