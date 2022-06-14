3e1b9379f5ed73776559f6c141ea1398ac60606c
istarter <najibkhan.bahrian@gmail.com>
2022-06-14 23:06:32 +0500

    dice compoenent

--- src/components/Die.js
+++ src/components/Die.js
@@ -2,12 +2,26 @@ import React from "react";
 
 const Die = (props) => {
   return (
-    <div>
-      <h1>{props.value}</h1>
+    <div style={styles.diceFace}>
+      <h1 className="dieNum">{props.value}</h1>
     </div>
   );
 };
 
 export default Die;
 
-// const styles = StyleSheet({});
+const styles = {
+  diceFace: {
+    height: "50px",
+    width: "50px",
+    boxShadow: "0px 2px 2px rgba(0, 0, 0, 0.15)",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: "10px",
+    backgroundColor: "white",
+  },
+  dieNum: {
+    fontSize: "2rem",
+  },
+};
--- src/components/Tenzies.js
+++ src/components/Tenzies.js
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Die from "./Die";
 import style from "./tenzies.css";
 
 const Tenzies = () => {
+  const [dice, setDice] = React.useState(allNewDice());
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push({ value: Math.ceil(Math.random() * 6), isHeld: false });
+    }
+    return newDice;
+  }
+
+  const diceElements = dice.map((die) => <Die value={die.value} />);
+
+  const rollDice = () => {
+    setDice(allNewDice());
+  };
+
   return (
     <main>
-      <div className="dice-container">
-        <Die value={"1"} />
-        <Die value={"1"} />
-        <Die value={"1"} />
-        <Die value={"1"} />
-        <Die value={"1"} />
-        <Die value={"1"} />
-        <Die value={"1"} />
-        <Die value={"1"} />
-        <Die value={"1"} />
-        <Die value={"1"} />
-      </div>
+      <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={rollDice}>
+        Roll
+      </button>
     </main>
   );
 };
